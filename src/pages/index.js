8eb1ebe2d9267bfa786e839c815d8dc6cefa5a55
Richard Williams <richardlwilliams@protonmail.com>
2021-07-07 11:30:06 -0600

    Add to paragraphs on main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 
-import pic1 from '../assets/images/pic01.jpg';
 import pic2 from '../assets/images/pic02.jpg';
 import pic3 from '../assets/images/pic03.jpg';
-import pic4 from '../assets/images/pic04.jpg';
 import pic5 from '../assets/images/pic05.jpg';
 import pic6 from '../assets/images/pic06.jpg';
 import pic7 from '../assets/images/pic07.jpg';
+import richjake from '../assets/images/rich_jake.jpg'
+import ngvsimage from '../assets/images/m61.jpg'
 
 import config from '../../config';
 const IndexPage = () => (
@@ -27,16 +27,12 @@ const IndexPage = () => (
       <section id="one" className="wrapper spotlight style1">
         <div className="inner">
           <a href="/#" className="image">
-            <img src={pic1} alt="" />
+            <img src={richjake} alt="" />
           </a>
           <div className="content">
-            <h2 className="major">Magna arcu feugiat</h2>
+            <h2 className="major">Who am I?</h2>
             <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
+              I'm a recent graduate from the computer science program at the University of Calgary looking to start a career as a software developer. I also have a degree in physics and earth science from the University of Victoria. I have experience working in resource exploration, underground utility locating, and the ski industry.
             </p>
             <a href="/#" className="special">
               Learn more
@@ -51,13 +47,9 @@ const IndexPage = () => (
             <img src={pic2} alt="" />
           </a>
           <div className="content">
-            <h2 className="major">Tempus adipiscing</h2>
+            <h2 className="major">What can I do?</h2>
             <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
+              I have a wide variety of experience in software development. Most of my experience so far has been building and deploying full-stack web applications. I know the front-end basics (HTML/CSS/Javascript) and have worked with frameworks such as React and Bootstrap. On the back-end I have built APIs written in both Javascript and Python. For databases I have experience with relational databases and noSQL databases. The languages I know best are Java, Javascript, Python, and C. I am also quite familier with linux.
             </p>
             <a href="/#" className="special">
               Learn more
@@ -72,7 +64,7 @@ const IndexPage = () => (
             <img src={pic3} alt="" />
           </a>
           <div className="content">
-            <h2 className="major">Nullam dignissim</h2>
+            <h2 className="major">Interests</h2>
             <p>
               Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
               turpis ante, nullam sit amet turpis non, sollicitudin posuere
@@ -89,22 +81,18 @@ const IndexPage = () => (
 
       <section id="four" className="wrapper alt style1">
         <div className="inner">
-          <h2 className="major">Vitae phasellus</h2>
+          <h2 className="major">Projects</h2>
           <p>
-            Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-            Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-            egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus
-            in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
+            Here are a few of the projects I've worked on recently.
           </p>
           <section className="features">
             <article>
               <a href="/#" className="image">
-                <img src={pic4} alt="" />
+                <img src={ngvsimage} alt="" />
               </a>
-              <h3 className="major">Sed feugiat lorem</h3>
+              <h3 className="major">NGVS Map</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                NGVS map is a web appication for astronomical data exploration and retrieval.
               </p>
               <a href="/#" className="special">
                 Learn more
@@ -114,10 +102,9 @@ const IndexPage = () => (
               <a href="/#" className="image">
                 <img src={pic5} alt="" />
               </a>
-              <h3 className="major">Nisl placerat</h3>
+              <h3 className="major">Inky.io</h3>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
+                Inky.io is an online multiplayer pictionary-like game.
               </p>
               <a href="/#" className="special">
                 Learn more
